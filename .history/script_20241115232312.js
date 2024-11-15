fe49ce26a9c9eb8b8273ecaad90d0aefb73bd93f
varsa-varniga <varsavarniga05@gmail.com>
2024-11-15 23:31:35 +0530

    initial commit

---
+++ .history/script_20241115232312.js
@@ -0,0 +1,20 @@
+
+//COUNTER PROGRAM
+/*const decrease=document.getElementById("d");
+const reset=document.getElementById("r");
+const increase=document.getElementById("i");
+
+const countbtn=document.getElementById("countbtn");
+let count=0;
+increase.onclick=function(){
+    count++;
+    countbtn.textContent=count;
+}
+decrease.onclick=function(){
+    count--;
+    countbtn.textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    countbtn.textContent=count;
+}*/
